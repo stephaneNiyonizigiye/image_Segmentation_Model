57438f04d89257049637e8eb1d49eb128867e496
stephaneNiyonizigiye <113949793+stephane-Git7@users.noreply.github.com>
2025-03-13 13:27:41 -0400

    test updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
+import sys
+import os
+
+# Ajouter le répertoire racine au PATH pour que 'app' soit détecté
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
 import pytest
-from app import app
+from app import app  # Assure-toi que 'app.py' est bien à la racine
 
 @pytest.fixture
 def client():
